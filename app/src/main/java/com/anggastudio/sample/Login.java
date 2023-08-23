6324ae48afb88c48efd6f97f57b2dc7733f59621
Melina <melina.llacua@gmail.com>
2023-08-23 12:43:47 -0500

    Utilizar las ApiRest

--- app/src/main/java/com/anggastudio/sample/Login.java
+++ app/src/main/java/com/anggastudio/sample/Login.java
@@ -91,7 +91,6 @@ public class Login extends AppCompatActivity {
         });
 
         /** Detectar el IMEI*/
-/**comentario*/
         imeii = findViewById(R.id.imei);
         imeii.setText(ObtenerIMEI.getDeviceId(getApplicationContext()));
 
